6aab540ffcb9b9672db82290ca4465c49fc562f6
venkat4097 <159035369+venkat4097@users.noreply.github.com>
2025-07-18 16:05:46 +0530

    Updated port for socket index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 // ✅ Initialize Socket.IO server
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", 
+    origin: "https://alumniconnect-frontend-h0h6.onrender.com", 
     credentials: true,
   },
 });
